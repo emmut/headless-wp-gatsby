551e6d3059cb3de70ba616fc56fcb3c1fa84c424
Emil Jansson <ejansson.swe@gmail.com>
2020-08-01 17:33:53 +0200

    test: config for gatsby graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,5 +49,17 @@ module.exports = {
         uri: 'https://emmut.io/wp/graphql',
       },
     },
+    // Simple config, passing URL
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: 'EMMUT',
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: 'emmut',
+        // Url to query from
+        url: 'https://emmut.io/wp/graphql',
+      },
+    },
   ],
 }
